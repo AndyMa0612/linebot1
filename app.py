18d694f42ded39021065d9d5d953de5b21f60ebb
AndyMa0612 <andy43049048@gmail.com>
2024-12-15 23:53:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('6nVcItOmiqSfD/1J+THWsTzvDOfVLHIhtrvlltLU+aNy6Zw9kzK3tZT6oDWs/86Tkronvv4mbchkTNeOKA0djpLLZRF9RiLOWcY7Fulm+hpb3bh/BGiR8bqvvqGxjpQ5HEDWSYfERJzetfN1IbKj8QdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('JMCPBQudpqEoC4PYuKzq5n5keshLPWyw1xVDWNL3w31JlRYHb6YxTAd7fhQaDqfZ38i3OEKUEVmPbKkA7d2qrSstvGVxxOvJXel+l6LdC8JXZKzZnzyj3Rdz5h5973ZozJxk84DoVHfeN2op+ONCCAdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('cb8a9cb5c70b1bed7adf4f521ed713b6')
+handler = WebhookHandler('2d76a3d9752771ac55a74d90e1ff6e2e')
 
-line_bot_api.push_message('U6a47d7e0a37f2e732e12675f7f8f1762', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U0b6441bd0ff804fe3f87793461cf615f', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -43,14 +43,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('告訴我秘密',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://www.campus-studio.com/download/flag.jpg',
-            preview_image_url='https://www.campus-studio.com/download/101.jpg'
+    if re.match('音樂片',message):
+        video_message = VideoSendMessage(
+            original_content_url='https://github.com/AndyMa0612/linebot1/raw/main/%E5%96%9C%E6%AD%A1%E4%BD%A0%E7%9C%9F%E7%9A%84%E5%A5%BD%E7%97%9B%E8%8B%A6.mp4',
+            preview_image_url='https://github.com/AndyMa0612/linebot1/raw/main/%E5%9C%96%E7%89%87.png'
         )
-        line_bot_api.reply_message(event.reply_token, image_message)
+        line_bot_api.reply_message(event.reply_token, video_message)
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="抱歉，沒有這類型的影片"))
+
 #主程式
 import os
 if __name__ == "__main__":
