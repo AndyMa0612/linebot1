728a02e58e6b8d55539a2e87adb10e5bad4f6c17
AndyMa0612 <andy43049048@gmail.com>
2024-12-16 17:50:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -42,40 +42,32 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = event.message.text
-    if re.match('推薦餐廳',message):
-        imagemap_message = ImagemapSendMessage(
-            base_url='https://i.imgur.com/N8lk6Zl.png',
-            alt_text='組圖訊息',
-            base_size=BaseSize(height=2000, width=2000),
+    message = text=event.message.text
+    if re.match('推薦景點',message):
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這是樣板傳送訊息',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/kNBl363.jpg',
+            title='中華民國',
+            text='選單功能－TemplateSendMessage',
             actions=[
-                URIImagemapAction(
-                    link_uri='https://www.popdaily.com.tw/search/%23%E6%97%A5%E5%BC%8F%E6%96%99%E7%90%86?filter=ALL&sort=RELEVANT',
-                    area=ImagemapArea(
-                        x=0, y=0, width=1000, height=1000
-                    )
+                PostbackAction(
+                    label='這是PostbackAction',
+                    display_text='顯示文字',
+                    data='實際資料'
                 ),
-                URIImagemapAction(
-                    link_uri='https://www.popdaily.com.tw/search/%23%E8%A5%BF%E5%BC%8F%E6%96%99%E7%90%86?filter=ALL&sort=RELEVANT',
-                    area=ImagemapArea(
-                        x=1000, y=0, width=1000, height=1000
-                    )
+                MessageAction(
+                    label='這是MessageAction',
+                    text='實際資料'
                 ),
-                URIImagemapAction(
-                    link_uri='https://www.popdaily.com.tw/search/%23%E4%B8%AD%E5%BC%8F%E6%96%99%E7%90%86?filter=ALL&sort=RELEVANT',
-                    area=ImagemapArea(
-                        x=0, y=1000, width=1000, height=1000
-                    )
-                ),
-                URIImagemapAction(
-                    link_uri='https://www.popdaily.com.tw/search/%23%E6%B3%95%E5%BC%8F%E6%96%99%E7%90%86?filter=ALL&sort=RELEVANT',
-                    area=ImagemapArea(
-                        x=1000, y=1000, width=1000, height=1000
-                    )
+                URIAction(
+                    label='這是URIAction',
+                    uri='https://en.wikipedia.org/wiki/Taiwan'
                 )
             ]
         )
-        line_bot_api.reply_message(event.reply_token, imagemap_message)
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
