c3ba59b2963462147e401d93eb1ff5604d9f730f
AndyMa0612 <andy43049048@gmail.com>
2024-12-19 01:03:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#載入LineBot所需要的套件
+# 載入 LineBot 所需要的套件
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,9 +13,9 @@ from linebot.models import *
 import re
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
+# 必須放上自己的 Channel Access Token
 line_bot_api = LineBotApi('uqn8F5AIftKxhnBJq+76T3YazN5AWYR+VxJrd68V6scH0EXgWzUzKiGMmGNXBn5u38i3OEKUEVmPbKkA7d2qrSstvGVxxOvJXel+l6LdC8KcxrlK1fuiS3C0iZu0CCV5ILLU1v50mud6jUaHkCOqSQdB04t89/1O/w1cDnyilFU=')
-# 必須放上自己的Channel Secret
+# 必須放上自己的 Channel Secret
 handler = WebhookHandler('2dfd9021dc688c45c6b5cbc9bf18cabc')
 
 line_bot_api.push_message('U0b6441bd0ff804fe3f87793461cf615f', TextSendMessage(text='你可以開始了'))
@@ -38,154 +38,128 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
+# 訊息傳遞區塊
+##### 基本上程式編輯都在這個 function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('查看菜單',message):
-        # Flex Message Simulator網頁：https://developers.line.biz/console/fx/
+    message = event.message.text
+    if re.match('查看菜單', message):
+        # Flex Message for the menu
         flex_message = FlexSendMessage(
-            alt_text='這是FlexSendMessage',
+            alt_text='餐廳推薦菜單',
             contents={
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://i.imgur.com/kNBl363.jpg",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover",
-    "action": {
-      "type": "uri",
-      "uri": "https://linecorp.com"
-    }
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "md",
-    "action": {
-      "type": "uri",
-      "uri": "https://linecorp.com"
-    },
-    "contents": [
-      {
-        "type": "text",
-        "text": "台灣熱門景點",
-        "size": "xl",
-        "weight": "bold"
-      },
-      {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
-              },
-              {
-                "type": "text",
-                "text": "台東3天2夜遊",
-                "weight": "bold",
-                "margin": "sm",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "NT 15000",
-                "size": "sm",
-                "align": "end",
-                "color": "#aaaaaa"
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
-              },
-              {
-                "type": "text",
-                "text": "花蓮3天2夜遊",
-                "weight": "bold",
-                "margin": "sm",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "NT 18000",
-                "size": "sm",
-                "align": "end",
-                "color": "#aaaaaa"
-              }
-            ]
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
-              },
-              {
-                "type": "text",
-                "text": "墾丁3天兩夜遊",
-                "weight": "bold",
-                "margin": "sm",
-                "flex": 0
-              },
-              {
-                "type": "text",
-                "text": "NT 16000",
-                "size": "sm",
-                "align": "end",
-                "color": "#aaaaaa"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "type": "text",
-        "text": "大腸包小腸、101、中正紀念堂",
-        "wrap": True,
-        "color": "#aaaaaa",
-        "size": "xxs"
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "button",
-        "style": "primary",
-        "color": "#905c44",
-        "margin": "xxl",
-        "action": {
-          "type": "uri",
-          "label": "馬上瀏覽",
-          "uri": "https://en.wikipedia.org/wiki/Taiwan"
-        }
-      }
-    ]
-  }
-}
+                "type": "carousel",
+                "contents": [
+                    {
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://i.imgur.com/eP1v8W9.jpg",  # Replace with actual image URL
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {"type": "text", "text": "大牛排", "weight": "bold", "size": "xl"},
+                                {"type": "text", "text": "多汁美味的頂級牛排，煎至完美熟度。", "wrap": True, "color": "#666666", "size": "sm"},
+                                {"type": "text", "text": "NT$ 880", "weight": "bold", "size": "lg", "color": "#FF0000"}
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "style": "primary",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "訂購",
+                                        "text": "您已成功將【大牛排】加入購物車"
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://i.imgur.com/4zO6C2n.jpg",  # Replace with actual image URL
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {"type": "text", "text": "海鮮義大利麵", "weight": "bold", "size": "xl"},
+                                {"type": "text", "text": "新鮮海鮮搭配濃郁醬汁，風味絕佳。", "wrap": True, "color": "#666666", "size": "sm"},
+                                {"type": "text", "text": "NT$ 520", "weight": "bold", "size": "lg", "color": "#FF0000"}
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "style": "primary",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "訂購",
+                                        "text": "您已成功將【海鮮義大利麵】加入購物車"
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://i.imgur.com/VmWhEFD.jpg",  # Replace with actual image URL
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {"type": "text", "text": "抹茶蛋糕", "weight": "bold", "size": "xl"},
+                                {"type": "text", "text": "香濃抹茶風味，搭配綿密蛋糕體。", "wrap": True, "color": "#666666", "size": "sm"},
+                                {"type": "text", "text": "NT$ 250", "weight": "bold", "size": "lg", "color": "#FF0000"}
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "style": "primary",
+                                    "action": {
+                                        "type": "message",
+                                        "label": "訂購",
+                                        "text": "您已成功將【抹茶蛋糕】加入購物車"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
         )
         line_bot_api.reply_message(event.reply_token, flex_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-#主程式
+
+# 主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
